3c21fef1636418ba0a595ec07961b74fdeba0582
pulsewave <mail@udolee.com>
2018-12-19 22:24:26 +0900

    add fresh public folder

---
+++ public/js/script.js
@@ -0,0 +1,38 @@
+
+$(document).ready(function(){
+	
+	
+	var hun = $('.header-col1');
+	var x = $('#overlay');
+	
+	hun.click(function() { 
+		$('#overlay').toggle();
+	});
+	
+	x.click(function() {
+		$('#overlay').hide();
+	});
+	
+	$(document).keyup(function(e) {
+
+  		if (e.keyCode == 27) { $('#overlay').hide(); }   // esc
+	});
+	
+	
+	
+	var photo = $('.next');
+	
+	$('.fadein img:gt(0)').hide();
+//	setInterval(function(){$('.fadein :first-child').hide().next('img').show().end().appendTo('.fadein');}, 1000);
+	photo.click(function(e){
+	$('.fadein :first-child').hide().next('img').show().end().appendTo('.fadein');
+//	
+
+
+	 e.stopPropagation();
+	});
+	
+	
+	
+	
+});
